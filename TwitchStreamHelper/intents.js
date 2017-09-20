47b5436e40fade38b3ed979883c2f77103035fbf
tyhilde <hildet@uw.edu>
2017-09-19 23:36:57 -0700

    added Intents file

---
+++ TwitchStreamHelper/intents.js
@@ -0,0 +1,180 @@
+{
+    "intents": [
+    {
+        "name": "AMAZON.CancelIntent",
+        "samples": []
+    },
+    {
+        "name": "AMAZON.HelpIntent",
+        "samples": []
+    },
+    {
+        "name": "AMAZON.StopIntent",
+        "samples": []
+    },
+    {
+        "name": "CatchAll",
+        "samples": [
+            "i might say something random",
+            "doo diddly",
+            "gibberish",
+            "snicker",
+            "doodle",
+            "whats that",
+            "i wonder",
+            "cool"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getFollowerCount",
+        "samples": [
+            "how many followers do I have",
+            "how many followers are there",
+            "how many people currently follow me",
+            "how many people follow me",
+            "how many people are following my stream",
+            "how many people are following me",
+            "how many people are following",
+            "what is my follower count",
+            "what's my follower count",
+            "what is my current follower count",
+            "what’s my current follower count",
+            "tell me my follower count",
+            "tell me how many people follow me",
+            "give me my follower count",
+            "get me my follower count"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getLastFiveFollowers",
+        "samples": [
+            "who were the last five followers",
+            "who were the last followers",
+            "who are my last followers",
+            "who are my last five follower",
+            "who were my last five followers",
+            "what are the names of the last five followers",
+            "what are the names of the last followers",
+            "tell me the last five followers",
+            "tell me the last followers",
+            "name the last five followers",
+            "name the last followers"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getLastFiveSubscribers",
+        "samples": [
+            "who were the last five subscribers",
+            "who were the last subscribers",
+            "who were the last subs",
+            "who are my last subscribers",
+            "who are my last five subscribers",
+            "who were my last five subscribers",
+            "what are the names of the last five subscribers",
+            "what are the names of the last subscribers",
+            "tell me the last five subscribers",
+            "tell me the last subscribers",
+            "tell me the last subs",
+            "name the last five subscribers",
+            "name the last subscribers"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getLastFollower",
+        "samples": [
+            "who just followed me",
+            "who just followed",
+            "who was my last follower",
+            "who was the last follower",
+            "whats the name of my last follower",
+            "whats the name of the last person who followed me",
+            "tell me the last follower",
+            "get the last follower"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getLastSubscriber",
+        "samples": [
+            "who just subscribed to me",
+            "who just subscribed",
+            "who was my last subscriber",
+            "who was the last subscriber",
+            "whats the name of my last subscriber",
+            "whats the name of the last person who subscribed to me",
+            "tell me the last subscriber",
+            "get the last subscriber"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getStreamUpTime",
+        "samples": [
+            "how long has my stream been live",
+            "how long have i been streaming",
+            "what's my up time",
+            "tell me the stream up time",
+            "whats my stream up time",
+            "get the up time",
+            "stream uptime"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getSubscriberCount",
+        "samples": [
+            "how many subscribers do I have",
+            "how many subs do I have",
+            "how many subscribers are there",
+            "how many people currently subscribe to me",
+            "how many people subscribe to me",
+            "how many people are subscribing my stream",
+            "how many people are subscribing",
+            "how many people are subs",
+            "what is my subscriber count",
+            "what's my subscriber count",
+            "what is my current subscriber count",
+            "what’s my current subscriber count",
+            "tell me my subscriber count",
+            "tell me how many people subscriber to me",
+            "give me my subscriber count",
+            "get me my subscriber count"
+        ],
+        "slots": []
+    },
+    {
+        "name": "getViewerCount",
+        "samples": [
+            "how many viewers do I have",
+            "how many viewers are there",
+            "how many people are watching",
+            "how many people are watching the stream",
+            "how many people are viewing my stream",
+            "what is my viewer count",
+            "what's my viewer count",
+            "what is my current viewer count",
+            "what’s my current viewer count",
+            "tell me my viewer count",
+            "tell me how many people are watching",
+            "give me my viewer count",
+            "get me my viewer count"
+        ],
+        "slots": []
+    },
+    {
+        "name": "isStreamLive",
+        "samples": [
+            "Is backshooters stream live",
+            "Is backshooter streaming",
+            "is the stream up",
+            "is backshooters twitch stream live",
+            "is stream live"
+        ],
+        "slots": []
+    }
+]
+}
